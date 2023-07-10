eed686eb100a271628803aff39990af8b8a6ed52
unknown <suranjanbehura8@gmail.com>
2023-07-10 17:11:53 +0530

    Initial commit

--- Demo.java
+++ Demo.java
@@ -1,5 +1,10 @@
 public class Demo{
     public static void main(String[] args) {
         System.out.println("Learn with Surya");
+        String s1="Surya";
+        String s2="Learn"
+        if(s1.equals(s2)){
+            System.out.println("Equal");
+        }
     }
 }
