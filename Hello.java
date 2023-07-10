31268e64c0136d9941ad204cb7a0a589cca72e01
unknown <suranjanbehura8@gmail.com>
2023-07-10 18:20:55 +0530

    BUG-134 changes

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@ public class Hello{
     public static void main(String[] args) {
         String s1="Pranab";
         String s2="Pranab";
-        String s3=s1.compareTo(s2);
+        //String s3=s1.compareTo(s2);
+        if(s1.compareTo(s2)){
+            System.out.println("Equal");
+        }
     }
 }
